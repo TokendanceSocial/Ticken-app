70923c668114d5b58acea322d1b5d196454e4837
dxiaoqi <1035038018@qq.com>
2023-02-12 13:57:53 +0800

    feat: console

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,5 @@ import './index.css';
 import './index.module.scss';
 import '@rainbow-me/rainbowkit/styles.css';
 const rootElement = document.getElementById('root');
-console.log('运行', process.env.TEAA);
 
 render(<App />, rootElement);
