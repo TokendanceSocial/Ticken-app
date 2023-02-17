c8f591eabec9443dea71cad4b03f6bc77ef54697
qinluhe <qinluhe.twodog@gmail.com>
2023-02-17 21:10:57 +0800

    fix: add redirect

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,15 @@
-import React, { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { Suspense, useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 export default function LayoutComponent() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (location.pathname === '/') {
+      navigate('/list');
+    }
+  }, [location, navigate]);
+
   return (
     <Suspense>
       <Outlet />
